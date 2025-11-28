2630e9b763b54e54698216299d5af0f3cba680de
Akash-V-P <akashvp.dev@gmail.com>
2025-11-28 19:17:55 +0530

    add login user endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,17 +4,24 @@ import { User } from '../models/user.model.js';
 import { uploadOnCloudinary } from '../utils/cloudinary.js';
 import { ApiResponse } from '../utils/ApiResponse.js'
 
+
+const generateAccessAndRefreshTokens = async(userId) => {
+    try {
+        const user = await User.findById(userId);
+        const accessToken = user.generateAccessToken;
+        const refreshToken = user.generateRefreshToken;
+
+        user.refreshToken = refreshToken;
+        await user.save({ validateBeforeSave: false });
+
+        return { accessToken, refreshToken };
+
+    } catch (error) {
+        throw new ApiError(500, "Something went wrong while generating access and refresh token");
+    }
+}
+
 const registerUser = asyncHandler(async (req, res) => {
-    //get user from frontend
-    //validation - not empty
-    //check if user already exist- email, username
-    // check for images/avatar
-    //upload them to cloudinary
-    //create user object
-    //create entry in db
-    //remove password and refresh token field from response
-    //check for user creation
-    //return response
 
     const { fullName, email, username, password } = req.body;
     // console.log("email: ",email);
@@ -62,7 +69,6 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(400, "Avatar file is required")
     }
 
-
     const user = await User.create(
         {
             username: username.toLowerCase(),
@@ -83,6 +89,94 @@ const registerUser = asyncHandler(async (req, res) => {
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User regestered successfully.")
     )
+
+    //get user from frontend
+    //validation - not empty
+    //check if user already exist- email, username
+    // check for images/avatar
+    //upload them to cloudinary
+    //create user object
+    //create entry in db
+    //remove password and refresh token field from response
+    //check for user creation
+    //return response
 })
 
-export { registerUser };
+const loginUser = asyncHandler(async (req, res) => {
+
+    const {email, username, password} = req.body;
+
+    if(!username || !email){
+        throw new ApiError(400, "Username or password is required");
+    }
+
+    const user = await User.findOne({$or:[{username}, {password}]});
+
+    if(!user){
+        throw new ApiError(404, "user does not exist");
+    }
+
+    const isPasswordValid = await user.isPasswordCorrect(password);
+
+    if(!isPasswordValid){
+        throw new ApiError(401, " Invalid user credentials");
+    }
+
+    const { accessToken, refreshToken } = await generateAccessAndRefreshTokens(user._id);
+
+    const loggedInUser = await User.findById(user._id).select("-password -refreshToken");
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .cookie("accessToken", accessToken, options)
+    .cookie("refreshToken", refreshToken, options)
+    .json(
+        new ApiResponse(
+            200,
+            {
+                user: loggedInUser, accessToken, refreshToken
+            },
+            "User logged in Successfully"
+        )
+    )
+
+
+    // get data from req body
+    // check for username/email
+    // find the user
+    // password check
+    // generate and refresh token to user
+    // send cookies
+})
+
+const logoutUser = asyncHandler(async (req, res) => {
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set: {
+                refreshToken: undefined
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out "))
+})
+
+export { registerUser, loginUser, logoutUser };
